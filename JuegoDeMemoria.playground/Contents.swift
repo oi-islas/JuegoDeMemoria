75c2ba9b51a8d1cb1f2380c996aae53d4e3a7c78
oi-islas <oislas@matco.com.mx>
2018-08-14 12:52:26 -0700

    Para Peer Review
    Semana 2

---
+++ JuegoDeMemoria.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var rango = 0...100
+
+for numero in rango {
+    
+    if(numero % 5 == 0){
+        print("#\(numero) Bingo!!!")
+    }
+    
+    if(numero % 2 == 0){
+        print("#\(numero) par!!!")
+    }
+    
+    if(numero % 2 != 0){
+        print("#\(numero) impar!!!")
+    }
+    
+    if numero >= 30 && numero <= 40{
+        print("# \(numero) Viva Swift!!!")
+        
+    }
+ 
+}
